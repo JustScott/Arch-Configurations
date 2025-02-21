07b5465711de70c18a5fd0157391196c985d5798
JustScott <development@justscott.me>
2025-02-21 08:28:11 -0600

    Add pretty output to install script
    In the process of adding "prettier" outputs

--- install.sh
+++ install.sh
@@ -21,29 +21,34 @@
 #  throughout the system.
 #
 
-if [[ -f $HOME/.bashrc ]]
+if [[ $(basename $PWD) != "Arch-Configurations" ]]
 then
-    ACTION="Source bashrc_extension in $HOME/.bashrc"
-    {
-        extension_path="$PWD/bashrc_extension"
-        cat $HOME/.bashrc | grep "source $extension_path" &>/dev/null || {
-            echo -e "\nsource $extension_path" >> $HOME/.bashrc
-        }
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    printf "\e[31m%s\e[0m\n" \
+        "[Error] Please run script from the Arch-Configurations base directory"
+    exit 1
 fi
+
+source ./shared_lib
+
+{
+    extension_path="$PWD/bashrc_extension"
+    if ! cat $HOME/.bashrc | grep "source $extension_path" &>/dev/null; then
+        echo -e "\nsource $extension_path" >> $HOME/.bashrc
+    fi
+} >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+task_output $! "$STDERR_LOG_PATH" "Source bashrc_extension in $HOME/.bashrc"
+[[ $? -ne 0 ]] && exit 1
 
 if { which nvim || type nvim; } &>/dev/null
 then
-    ACTION="Create soft links for '$HOME/.vimrc' and '$HOME/.config/nvim'"
     {
         mkdir -p $HOME/.config/nvim
         ln -sf $PWD/init.vim $HOME/.config/nvim
         ln -sf $PWD/init.vim $HOME/.vimrc
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-            && echo "[SUCCESS] $ACTION" \
-            || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" \
+        "Create soft links for '$HOME/.vimrc' and '$HOME/.config/nvim'"
+    [[ $? -ne 0 ]] && exit 1
 
     #
     #  (note) You must run `:PlugInstall` from vim or neovim to install
@@ -53,154 +58,142 @@ then
     if ! [ -f $HOME/.local/share/nvim/site/autoload/plug.vim ]
     then
         mkdir -p $HOME/.local/share/nvim/site/autoload
-        ACTION="Download & Install vim-plug"
-        echo -n "...$ACTION..."
         {
             curl -Lo $HOME/.local/share/nvim/site/autoload/plug.vim \
                 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
             nvim -c "PlugInstall | qall" --headless
-        } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-            && echo "[SUCCESS]" \
-            || { "[FAIL] wrote error log to ~/miniarcherrors.log"; exit; }
+        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        task_output $! "$STDERR_LOG_PATH" "Download & Install vim-plug"
+        [[ $? -ne 0 ]] && exit 1
     fi
 fi
 
 if { which git || type git; } &>/dev/null
 then
-    ACTION="Configure global git user defaults"
     {
         git config --global user.name JustScott
         git config --global user.email development@justscott.me
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Configure global git user defaults"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 # We can presume X to be running if wayland isnt, and the $DISPLAY variable has value
 if [[ -z $WAYLAND_DISPLAY && $DISPLAY ]]
 then
-    ACTION="Create soft link to $HOME/.xinitrc"
-    ln -sf $PWD/.xinitrc $HOME/.xinitrc >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    ln -sf $PWD/.xinitrc $HOME/.xinitrc >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to $HOME/.xinitrc"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which river || type river; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/river/init'"
     {
         mkdir -p $HOME/.config/river
         ln -sf $PWD/river/init $HOME/.config/river/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/river/init'"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which foot || type foot; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/foot/foot.ini'"
     {
         mkdir -p $HOME/.config/foot
         ln -sf $PWD/foot/foot.ini $HOME/.config/foot/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/foot/foot.ini'"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which lf || type lf; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/lf/{lfrc,previewer.sh}'"
     {
         mkdir -p $HOME/.config/lf
         ln -sf $PWD/lf/{lfrc,previewer.sh} $HOME/.config/lf/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/lf/{lfrc,previewer.sh}'"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which lynx || type lynx; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/lynx/lynx.cfg'"
     {
         mkdir -p $HOME/.config/lynx
         ln -sf $PWD/lynx/lynx.cfg $HOME/.config/lynx/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/lynx/lynx.cfg'"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which calcurse || type calcurse; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/calcurse/conf'"
     {
         mkdir -p $HOME/.config/calcurse
         ln -sf $PWD/calcurse/conf $HOME/.config/calcurse/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/calcurse/conf'"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which picom || type picom; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/picom/picom.conf'"
     {
         mkdir -p $HOME/.config/picom
         ln -sf $PWD/picom/picom.conf $HOME/.config/picom
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/picom/picom.conf'"
+    [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which bat || type bat; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/bat/config'"
     {
         mkdir -p $HOME/.config/bat
         ln -sf $PWD/bat/config $HOME/.config/bat/config
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" "Create soft link to '$HOME/.config/bat/config'"
+    [[ $? -ne 0 ]] && exit 1
 
 
     if { which git || type git; } &>/dev/null
     then
-        ACTION="Configure global git pager as bat"
         git config --global core.pager "bat --paging=always --style=changes" \
-            >/dev/null 2>>/tmp/archconfigurationerrors.log \
-                && echo "[SUCCESS] $ACTION" \
-                || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+            >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        task_output $! "$STDERR_LOG_PATH" "Configure global git pager as bat"
+        [[ $? -ne 0 ]] && exit 1
     fi
 fi
 
 if { which newsboat || type newsboat; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/newsboat/{config,urls}'"
     {
         mkdir -p $HOME/.config/newsboat
         ln -sf $PWD/newsboat/{config,urls} $HOME/.config/newsboat/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        task_output $! "$STDERR_LOG_PATH" \
+            "Create soft link to '$HOME/.config/newsboat/{config,urls}'"
+        [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which mpv || type mpv; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/mpv/mpv.conf'"
     {
         mkdir -p $HOME/.config/mpv
         ln -sf $PWD/mpv/mpv.conf $HOME/.config/mpv/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        task_output $! "$STDERR_LOG_PATH" \
+            "Create soft link to '$HOME/.config/mpv/mpv.conf'"
+        [[ $? -ne 0 ]] && exit 1
 fi
 
 if { which ytfzf || type ytfzf; } &>/dev/null
 then
-    ACTION="Create soft link to '$HOME/.config/ytfzf/conf.sh'"
     {
         mkdir -p $HOME/.config/ytfzf
         ln -sf $PWD/ytfzf/conf.sh $HOME/.config/ytfzf/
-    } >/dev/null 2>>/tmp/archconfigurationerrors.log \
-        && echo "[SUCCESS] $ACTION" \
-        || echo "[FAIL] $ACTION... wrote error log to /tmp/archconfigurationerrors.log"
+    } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        task_output $! "$STDERR_LOG_PATH" \
+            "Create soft link to '$HOME/.config/ytfzf/conf.sh'"
+        [[ $? -ne 0 ]] && exit 1
 fi
